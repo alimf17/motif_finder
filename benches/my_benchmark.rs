ba83f6add37f93f6c7abbe3a39795e68b55af96d
alimf17 <alimf@umich.edu>
2023-04-14 16:32:37 -0400

    Implemented a return bind score

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,7 +1,7 @@
-extern crate motif_finder;
+
 use motif_finder::sequence::seq::Sequence;
 use rand::Rng;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
 
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -13,12 +13,12 @@ fn criterion_benchmark(c: &mut Criterion) {
     let block_lens: Vec<usize> = (0..280).map(|_| 3125).collect();
     let sequence: Sequence = Sequence::new_manual(blocks, block_inds, start_bases,block_lens);
 
-    let nbases = 15;
-    for block in 0..280 {
-        for ind in 0..(3125-nbases) {
-            c.bench_function("blockT", |b| b.iter(|| sequence.return_bases(block, ind, nbases)));
-        }
-    }
+    let nbases = 8;
+    let block = (0..28).collect::<Vec<_>>(); 
+    let inds = (0..(3125-nbases)).collect::<Vec<_>>();
+    
+    c.bench_function("blockT", |b| b.iter(|| block.iter().zip(inds.iter()).map(|(&l, &i)| sequence.return_bases(l, i, nbases)).collect::<Vec<_>>()));        
+    
 }
 
 criterion_group!(benches, criterion_benchmark);
