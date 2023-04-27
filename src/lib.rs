79952138203d3d520cf913e02579a359476d56f1
Elvana Hammoud <afarhat@MacBook-Pro-75.local>
2023-04-27 15:03:02 -0400

    Made implementations easier for HMC stuff. NOTE: Need to fix rand_new() to actually pull from dirichlet

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@ pub mod sequence;
 pub mod waveform;
 
 
-use crate::base::bases::Base;
+/*use crate::base::bases::Base;
 use crate::base::bases::GBase;
 use crate::base::bases::TruncatedLogNormal;
-use crate::base::bases::Motif;
+use crate::base::bases::Motif;*/
+
+use crate::base::*;
 use crate::sequence::seq::Sequence;
 use statrs::distribution::{Continuous, ContinuousCDF, LogNormal, Normal};
 use statrs::statistics::{Min, Max};
