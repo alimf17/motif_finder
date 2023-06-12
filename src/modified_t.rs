0cc1246cce9af3366217a6045f6604f0d627066c
Ali Farhat <alimf@umich.edu>
2023-06-12 11:17:23 -0400

    Theoretically implemented the enum for the background dist: not computing correctly, currently

--- src/modified_t.rs
+++ src/modified_t.rs
@@ -2,54 +2,169 @@ use rand::Rng;
 use rand::seq::SliceRandom;
 use rand::distributions::{Distribution, Uniform, WeightedIndex};
 use statrs::distribution::{Continuous, ContinuousCDF, LogNormal, Normal, Dirichlet, Exp};
-use statrs::function::{gamma, erf}
+use statrs::function::{gamma::*, erf::*};
 use statrs::statistics::{Min, Max, Distribution as OtherDistribution};
 use num_traits::Float;
 
+use serde::{Serialize, Deserialize};
 
+//I apologize for this struct in advance, in particular, the FastT implementation.
+//It is an unholy combination of R's background C implementation of pt, modified 
+//and pared down for my use, and the statrs implementation of things like the gamma 
+//function. You'll notice that my code is optimized for getting ln densities. This
+//is intentional: we can end up with very small densities and precision is more
+//critical for our purposes at the ends of our distribution
+
+const LN_GAMMA_3_HALFS: f64 = -0.120782237635245222;
+const M_SQRT_PI: f64 = 1.772453850905516027298167483341;
+const NUM_BGRAT_TERMS: usize = 30;
+
+const GIVE_UP_AND_USE_NORMAL: f64 = 20.0;
 
 pub enum BackgroundDist {
     Normal(Normal),
     FastT(FastT),
 }
 
+impl BackgroundDist {
+
+    pub fn new(scale: f64, freedom: f64) -> BackgroundDist {
+
+        if scale < 0.0 {
+            panic!("Only give me positive scale parameters!");
+        }
+        if freedom <= 2.0 { //We need at least a variance to be well defined. There's no working with this, otherwise
+            panic!("This background distribution is too ill behaved to do inference on it!");
+        } else if freedom >= GIVE_UP_AND_USE_NORMAL {
+            BackgroundDist::Normal(Normal::new(0.0, scale*(freedom/(freedom-2.)).sqrt()).expect("This shouldn't be possible."))
+        } else {
+            BackgroundDist::FastT(FastT{ scale: scale, freedom: freedom})
+        }
+    }
+    pub fn ln_cd_and_sf(&self,x: f64) -> (f64, f64) {
+        match self {
+            BackgroundDist::Normal(norm) => (norm.cdf(x).ln(), norm.sf(x).ln()),
+            BackgroundDist::FastT(fast) => fast.ln_cd_and_sf(x)
+        }
+    }
+}
+
+impl ContinuousCDF<f64, f64> for BackgroundDist {
+
+    fn cdf(&self, x: f64) -> f64 {
+        match self {
+            BackgroundDist::Normal(norm) => norm.cdf(x),
+            BackgroundDist::FastT(fast) => fast.cdf(x)
+        }
+    }
+
+    fn sf(&self, x: f64) -> f64 {
+        match self {
+            BackgroundDist::Normal(norm) => norm.sf(x),
+            BackgroundDist::FastT(fast) => fast.sf(x)
+        }
+    }
+}
+
+impl ContinuousLnCDF<f64, f64> for BackgroundDist {
+
+
+    fn ln_cdf(&self, x: f64) -> f64{ 
+        match self {
+            BackgroundDist::Normal(norm) => norm.cdf(x).ln(),
+            BackgroundDist::FastT(fast) => fast.ln_cdf(x)
+        }
+    }
+
+    fn ln_sf(&self, x: f64) -> f64 {
+        match self {
+            BackgroundDist::Normal(norm) => norm.sf(x).ln(),
+            BackgroundDist::FastT(fast) => fast.ln_sf(x)
+        }
+    }
+}
+
+impl Continuous<f64, f64> for BackgroundDist {
+
+    fn pdf(&self, x: f64) -> f64{
+        match self {
+            BackgroundDist::Normal(norm) => norm.pdf(x),
+            BackgroundDist::FastT(fast) => fast.pdf(x)
+        }
+    }
+    fn ln_pdf(&self, x: f64) -> f64{
+        match self {
+            BackgroundDist::Normal(norm) => norm.ln_pdf(x),
+            BackgroundDist::FastT(fast) => fast.ln_pdf(x)
+        }
+    }
+    
+}
+impl Min<f64> for BackgroundDist {
+    fn min(&self) -> f64 {
+        return -f64::INFINITY
+    }
+}
+impl Max<f64> for BackgroundDist {
+    fn max(&self) -> f64 {
+        return f64::INFINITY
+    }
+}
+#[derive(Serialize, Deserialize)]
 struct FastT {
     scale: f64,
     freedom: f64,
 }
 
-const LN_GAMMA_3_HALFS = -0.120782237635245222;
-
 impl ContinuousCDF<f64, f64> for FastT {
 
 
-    fn cdf(&self, calc: f64) -> f64{
-        todo!()
+    fn cdf(&self, x: f64) -> f64{
+        self.ln_cd_and_sf(x).0.exp()
     }
 
-    fn sf(&self, calc: f64) -> f64 {
-        todo!()
+    fn sf(&self, x: f64) -> f64 {
+        self.ln_cd_and_sf(x).1.exp()
     }
 }
 
 impl ContinuousLnCDF<f64, f64> for FastT {
 
 
-    fn ln_cdf(&self, calc: f64) -> f64{
-        todo!()
+    fn ln_cdf(&self, x: f64) -> f64{
+        self.ln_cd_and_sf(x).0
     }
 
-    fn ln_sf(&self, calc: f64) -> f64 {
-        todo!()
+    fn ln_sf(&self, x: f64) -> f64 {
+        self.ln_cd_and_sf(x).1
     }
 }
 impl Continuous<f64, f64> for FastT {
 
-    fn pdf(&self, calc: f64) -> f64{
-        todo!()
+    fn pdf(&self, x: f64) -> f64{
+        if x.is_infinite() {
+            0.0
+        } else { //We don't implement the code to give up and use a normal distribution because we only access FastT when we know we aren't giving up 
+            let d = x / self.scale;
+            (ln_gamma((self.freedom + 1.0) / 2.0) - ln_gamma(self.freedom / 2.0))
+                .exp()
+                * (1.0 + d * d / self.freedom).powf(-0.5 * (self.freedom + 1.0))
+                / (self.freedom * std::f64::consts::PI).sqrt()
+                / self.scale
+        }
+
     }
-    fn ln_pdf(&self, calc: f64) -> f64 {
-        todo!()
+    fn ln_pdf(&self, x: f64) -> f64 {
+        if x.is_infinite() {
+            f64::NEG_INFINITY
+        } else {
+            let d = x / self.scale;
+            ln_gamma((self.freedom + 1.0) / 2.0)
+                - 0.5 * ((self.freedom + 1.0) * (1.0 + d * d / self.freedom).ln())
+                - ln_gamma(self.freedom / 2.0)
+                - 0.5 * (self.freedom * std::f64::consts::PI).ln()
+                - self.scale.ln()
+        }
     }
 
 }
@@ -75,30 +190,35 @@ trait ContinuousLnCDF<K: Float, T: Float>: Min<K> + Max<K> {
 
 impl FastT {
 
-    fn ln_cd_and_sf(&self,x: f64) -> (f64, f64) {
+    pub fn ln_cd_and_sf(&self,x: f64) -> (f64, f64) {
 
         let q2 = (x/self.scale).powi(2);
 
+        let b = self.scale/2.;
+
         let major_branch = q2 >= self.freedom;
 
+        //inp is technically always mathematically the same thing, but we calculate it slightly 
+        //differently because theoretically the same and numerically the same aren't the same thing
         let inp = if major_branch {1./(1.+q2/self.scale)} else { self.scale/(self.scale+q2)};
 
         if major_branch {
-            let w = modified_bpser(self.scale/2, 1/2, inp, 1e-14, true);
-            (w, ln_1p(-w.exp()))
+            let w = modified_bpser(b, inp, 1e-14);
+            return (w, (-w.exp()).ln_1p());
         }
 
         if inp >= 0.29 {
-            let w1 = modified_bpser(self.scale/2, 1/2, inp, 1e-14, true);
-            (ln_1p(-w1.exp()), w)
+            let w1 = modified_bpser(b, inp, 1e-14);
+            return ((-w1.exp()).ln_1p(), w1);
         }
 
         let n = 20;
 
-        let mut w1 = bup(self.scale/2., inp, 1e-15, n, true);
+        let mut w1 = modified_bup(b, inp, 1e-14, n);
 
+        w1 = modified_bgrat(b+(n as f64), inp, w1, 1e-14);
 
-
+        ((-w1.exp()).ln_1p(), w1)
     }
 
 }
@@ -113,41 +233,37 @@ impl FastT {
 //Also, we are using the ln_gamma function from statrs instead of 
 //the official R implementation: it's accurate up to about 6 bits at
 //the very end of our floats and takes ~60ns regardless of input
-fn modified_bpser(b: f64, x: f64, eps: f64, get_ln: bool) -> f64 {
+fn modified_bpser(b: f64, x: f64, eps: f64) -> f64 {
 
-    if (x == 0) { if ln_dens {-f64:INFINITY } else {0.0} }
-    if( x.is_nan() ) {f64::NAN}
+    if x == 0. {return -f64::INFINITY;  }
+    if( x.is_nan() ) {return f64::NAN; }
 
     let a0 = 0.5_f64.min(b);
     let b0 = 0.5_f64.max(b);
  
-    let ln_dens = x.ln()/2.-ln_beta_with_3_halfs(b0)
+    let ln_dens = x.ln()/2.-ln_beta_with_3_halfs(b0);
 
     let mut sum = 0.0;
     let mut c = 0.0;
     let mut n = 0.;
-    let mut w = f64:INFINITY;
+    let mut w = f64::INFINITY;
 
     let tol = eps/2.;
 
     while (n < 1e7) && (w.abs() > tol) {
         n+=1.0;
-        c *= (0.5 - b / n + 0.5) * x;
-        w = c / (a + n);
+        c *= (0.5 - (b/n) + 0.5) * x;
+        w = c / (0.5 + n);
         sum += w;
     }
 
-    if get_len {
-        ln_dens+(sum/2.).ln_1p()
-    } else {
-        ln_dens.exp()*((sum/2.+1.))
-    }
+    ln_dens+(sum/2.).ln_1p()
 
     //implemented
 }
 
 
-fn modified_bup(a: f64, x:f64, eps: f64, n: usize, get_ln: bool) -> f64 {
+fn modified_bup(a: f64, x:f64, eps: f64, n: usize) -> f64 {
 
     let rem = a-a.floor();
     let retval = modified_brcmp(rem, x);
@@ -160,7 +276,7 @@ fn modified_bup(a: f64, x:f64, eps: f64, n: usize, get_ln: bool) -> f64 {
 
     for i in 0..(n-1) {
 
-        l = i as f64;
+        let l = i as f64;
         d *= x*(apb+l)/(ap1+l);
         w+=d;
         if (d <= eps * w) {
@@ -169,19 +285,24 @@ fn modified_bup(a: f64, x:f64, eps: f64, n: usize, get_ln: bool) -> f64 {
 
     }
 
-    if get_ln {
-        ret_val+w
-    } else {
-        (ret_val+w).exp()
-    }
+    retval+w
 
     //implemented
 
 }
 
-fn modified_bgrat(a: f64, x: f64, w: f64, eps: f64, get_ln: bool) -> f64 {
+
+
+fn modified_bgrat(a: f64, x: f64, w: f64, eps: f64) -> f64 {
+
     let nu = (a-1.)/2.+0.5;
-    let z = -nu * x.ln();
+    let lnx = x.ln();
+    let z = -nu * lnx;
+
+    let b = 0.5_f64;
+
+    let mut c = [0.0_f64; NUM_BGRAT_TERMS];
+    let mut d = [0.0_f64; NUM_BGRAT_TERMS];
 
     let log_r = a.ln() - ln_gamma(a+1.) + a * z.ln() - z;
     let log_u = log_r - (ln_beta_with_3_halfs(a)-LN_GAMMA_3_HALFS + a * nu.ln());
@@ -189,10 +310,40 @@ fn modified_bgrat(a: f64, x: f64, w: f64, eps: f64, get_ln: bool) -> f64 {
 
     let mut wp = w;
 
-    todo!();
+    let q_r = grat_r(z, log_r);
+    let v = 0.25 / (nu * nu);
+	let t2 = lnx * 0.25 * lnx;
+	let mut j = q_r;
+	let mut sum = j;
+	let mut t = 1.;
+    let mut cn = 1.;
+    let mut n2 = 0.;
 
 
+    for n in 0..NUM_BGRAT_TERMS {
 
+        let np1 = (n+1) as f64;
+        let bp2n = b + n2;
+	    j = (bp2n * (bp2n + 1.) * j + (z + bp2n + 1.) * t) * v;
+	    n2 += 2.;
+	    t *= t2;
+	    cn /= n2 * (n2 + 1.);
+	    c[n] = cn;
+        let mut s: f64 = 0.;
+        if (n > 0) {
+            let mut coef = b - np1;
+            for i in 1..=n {
+                s += coef * c[i - 1] * d[n - i];
+                coef += b;
+            }
+        }
+        d[n] = (b-1.) * cn + s / np1;
+        sum += (d[n] * j);
+
+
+    }
+
+    logspace_add(w, log_u+sum.ln())
 }
 
 //Panics: x not an element of (0,1)
@@ -202,13 +353,14 @@ fn modified_brcmp(a: f64, x: f64) -> f64 {
     let b0 = 0.5_f64.max(a);
 
 
+
     let lnx = x.ln();
     let ln1mx = (-x).ln_1p();
  
     
-    let ans = a*lnx+b*ln1mx-ln_beta_with_3_halfs(a);
+    let ans = a*lnx+0.5*ln1mx-ln_beta_with_3_halfs(a);
 
-    ans+a0.ln()-(a0/b0).ln_1p();
+    ans+a0.ln()-(a0/b0).ln_1p()
 
     //implemented
 
@@ -218,4 +370,76 @@ fn ln_beta_with_3_halfs(a: f64) -> f64 {
     LN_GAMMA_3_HALFS+ln_gamma(a+1.)-ln_gamma(a+1.5)
 }
 
+fn grat_r(x: f64, log_r: f64) -> f64{
+    let sx = x.sqrt();
+    if x < 0.25 {
+        (1.0-erf(sx))*(-log_r).exp()
+    } else {
+        erfc(sx) * M_SQRT_PI * x.exp()/sx
+    }
 
+}
+
+fn logspace_add(lnx: f64, lny: f64) -> f64 {
+    lnx.max(lny)+(-(lnx-lny).abs()).exp().ln_1p()
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum BackgroundDistDef {
+    Normal(NormalDef),
+    FastT(FastT),
+}
+
+impl From<BackgroundDistDef> for BackgroundDist {
+    fn from(def: BackgroundDistDef) -> BackgroundDist {
+        match def {
+            BackgroundDistDef::Normal(norm) => BackgroundDist::Normal(Normal::new(norm.mean, norm.std_dev).unwrap()),
+            BackgroundDistDef::FastT(fast) => BackgroundDist::FastT(fast),
+        }
+    }
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct NormalDef {
+    mean: f64,
+    std_dev: f64,
+}
+
+impl From<NormalDef> for Normal {
+    fn from(def: NormalDef) -> Normal {
+        Normal::new(def.mean, def.std_dev).unwrap()
+    }
+}
+/*
+trait Getter {
+    fn get_mean(&self) -> f64;
+    fn get_sd(&self) -> f64;
+}
+
+impl Getter for Normal {
+    fn get_mean(&self) -> f64 { self.mean().unwrap() }
+    fn get_sd(&self) -> f64 { self.std_dev().unwrap() }
+}*/
+
+
+
+#[cfg(test)]
+mod tests{
+
+    use crate::modified_t::BackgroundDist;
+    #[test]
+    fn attempt() {
+
+        let dis = BackgroundDist::new(1.0, 2.84);
+
+        let data = [-0.3, 0.3, 4.5, -2.9];
+
+        for dat in data.iter() {
+            let (c, s) = dis.ln_cd_and_sf(*dat);
+            println!("cd {}, sf {}",c, s);
+        }
+
+
+    }
+}
