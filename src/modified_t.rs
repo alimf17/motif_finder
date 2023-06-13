19a3d3da4e7b87482d84dcc48dc92538734a9568
Ali Farhat <alimf@umich.edu>
2023-06-13 13:04:40 -0400

    Implemented my version of R's t test function

--- src/modified_t.rs
+++ src/modified_t.rs
@@ -15,6 +15,8 @@ use serde::{Serialize, Deserialize};
 //is intentional: we can end up with very small densities and precision is more
 //critical for our purposes at the ends of our distribution
 
+
+const LN_GAMMA_HALF: f64 = 0.5723649429247000870717136756;
 const LN_GAMMA_3_HALFS: f64 = -0.120782237635245222;
 const M_SQRT_PI: f64 = 1.772453850905516027298167483341;
 const NUM_BGRAT_TERMS: usize = 30;
@@ -195,34 +197,28 @@ impl FastT {
 
         let q2 = (x/self.scale).powi(2);
 
-        let b = self.freedom/2.;
+        let a = self.freedom/2.;
 
         let major_branch = q2 >= self.freedom;
 
         //inp is technically always mathematically the same thing, but we calculate it slightly 
         //differently because theoretically the same and numerically the same aren't the same thing
         let inp = if major_branch {1./(1.+q2/self.freedom)} else { self.freedom/(self.freedom+q2)};
+                    //This will always be <= 0.5                    This will always be >= 0.5
+                    //  (no swap)                                   (swap)
+                    //So in both cases, once incorporating the swap, a = self.freedom/2., b = 0.5
+                    //The only difference in the swap is that the latter requires 1.-inp
+                    //And because b = 0.5 < 1, we always take the bpser route in the code
+                    //Wow, the C code is written arcanely
 
-        let (prew, prew1) = if major_branch {
-            let w = modified_bpser(0.5, b, inp, 1e-14);
-            let w_alt = bpser(0.5, b, inp, 1e-16);
-            println!("mb: {}, b: {}", w, w_alt);
-            (w, (-w.exp()).ln_1p())
-        } else if inp >= 0.29 {
-            let w1 = modified_bpser(b, 0.5, 1.0-inp, 1e-14);
-            ((-w1.exp()).ln_1p(), w1)
-        } else {
-            let n = 20;
-            let mut w1 = modified_bup(b, inp, 1e-14, n);
-            w1 = modified_bgrat(b+(n as f64), inp, w1, 1e-14);
-            ((-w1.exp()).ln_1p(), w1)
-        };
 
-        println!("x: {}, q2: {}, inp: {}, b: {}, Ix: {}, Ixf: {},prew: {}, prew1: {}", x, q2,inp, b,beta_inc(0.5, b, inp)/beta(0.5, b), beta_inc(b, 0.5, inp)/beta(b,0.5),prew, prew1);
+        let prew = bpser(a, inp, 1e-14);
+
+        println!("x: {}, q2: {}, inp: {}, b: {}, Ix: {}, Ixf: {},prew: {}, prew1: {}", x, q2,inp, a,beta_inc(a, 0.5, inp)/beta(a,0.5), beta_inc(a, 0.5, inp)/beta(a,0.5),prew, prew);
         if (x > 0.0) {
-            ((-0.5*(prew.exp())).ln_1p(), prew1-M_LN_2)
+            ((-0.5*(prew.exp())).ln_1p(), prew-M_LN_2)
         } else {
-            (prew1-M_LN_2, (-0.5*prew.exp()).ln_1p())
+            (prew-M_LN_2, (-0.5*prew.exp()).ln_1p())
         }
        
     }
@@ -235,48 +231,17 @@ impl FastT {
 //It is attenuated because we need it for very particular
 //use cases
 
-//In particular, when we use it, one of the values is always 1/2
+//In particular, when we use it, b = 0.5
 //Also, we are using the ln_gamma function from statrs instead of 
 //the official R implementation: it's accurate up to about 6 bits at
 //the very end of our floats and takes ~60ns regardless of input
-fn modified_bpser(a: f64, b: f64, x: f64, eps: f64) -> f64 {
 
-    if x == 0. {return -f64::INFINITY;  }
-    if( x.is_nan() ) {return f64::NAN; }
+fn bpser(a: f64, x: f64, eps: f64) -> f64 {
 
-    let a0 = a.min(b);
-    let b0 = a.max(b);
- 
-    let ln_dens = x.ln()/2.-ln_beta(a0,b0);
 
-    let mut sum = 0.0;
-    let mut c = 0.0;
-    let mut n = 0.;
-    let mut w = f64::INFINITY;
 
-    let tol = eps/2.;
+    let mut ans: f64 = a * x.ln()-ln_beta_half_times_a(a);
 
-    while (n < 1e7) && (w.abs() > tol) {
-        n+=1.0;
-        c *= (0.5 - (b/n) + 0.5) * x;
-        w = c / (a + n);
-        sum += w;
-    }
-
-    ln_dens+(sum/2.).ln_1p()
-
-    //implemented
-}
-
-fn bpser(a: f64, b: f64, x: f64, eps: f64) -> f64 {
-
-    let a0 = a.min(b);
-
-    let b0 = a.max(b);
-
-    let mut ans: f64 = a * x.ln()-ln_gamma(a+1.)-ln_gamma(b) + ln_gamma(a+b);
-
-    println!("preser: {}, x: {}, a: {}, b: {}", ans, x, a, b);
 
     /* ----------------------------------------------------------------------- */
     /*		       COMPUTE THE SERIES */
@@ -288,14 +253,10 @@ fn bpser(a: f64, b: f64, x: f64, eps: f64) -> f64 {
     let mut w: f64 = f64::INFINITY;
     while (n < 1e7 && w.abs() > tol) { // sum is alternating as long as n < b (<==> 1 - b/n < 0)
         n += 1.;
-        c *= (0.5 - (b / n)+0.5 ) * x;
+        c *= (0.5 - (0.5 / n)+0.5 ) * x;
         w = c / (a + n);
         sum += w;
     }     
-    if(w.abs() > tol) { // the series did not converge (in time)
-                        // warn only when the result seems to matter:
-        warn!("bpser ans doesn't seem to have converged");
-    }
     
     if (a*sum > -1.) {ans += (a * sum).ln_1p();}
     else {
@@ -304,115 +265,13 @@ fn bpser(a: f64, b: f64, x: f64, eps: f64) -> f64 {
     return ans;
 } /* bpser */
 
-fn modified_bup(a: f64, x:f64, eps: f64, n: usize) -> f64 {
-
-    let rem = a-a.floor();
-    let retval = modified_brcmp(rem, x);
-
-    let apb = a+0.5;
-    let ap1 = a+1.;
-
-    let mut w = 1.0_f64;
-    let mut d = 1.;
-
-    for i in 0..(n-1) {
-
-        let l = i as f64;
-        d *= x*(apb+l)/(ap1+l);
-        w+=d;
-        if (d <= eps * w) {
-            break;
-        }
-
-    }
-
-    retval+w
-
-        //implemented
-
-}
-
-
-
-fn modified_bgrat(a: f64, x: f64, w: f64, eps: f64) -> f64 {
-
-    let nu = (a-1.)/2.+0.5;
-    let lnx = x.ln();
-    let z = -nu * lnx;
-
-    let b = 0.5_f64;
-
-    let mut c = [0.0_f64; NUM_BGRAT_TERMS];
-    let mut d = [0.0_f64; NUM_BGRAT_TERMS];
-
-    let log_r = a.ln() - ln_gamma(a+1.) + a * z.ln() - z;
-    let log_u = log_r - (ln_beta_with_3_halfs(a)-LN_GAMMA_3_HALFS + a * nu.ln());
-    let u = log_u.exp();
-
-    let mut wp = w;
-
-    let q_r = grat_r(z, log_r);
-    let v = 0.25 / (nu * nu);
-	let t2 = lnx * 0.25 * lnx;
-	let mut j = q_r;
-	let mut sum = j;
-	let mut t = 1.;
-    let mut cn = 1.;
-    let mut n2 = 0.;
-
-
-    for n in 0..NUM_BGRAT_TERMS {
-
-        let np1 = (n+1) as f64;
-        let bp2n = b + n2;
-	    j = (bp2n * (bp2n + 1.) * j + (z + bp2n + 1.) * t) * v;
-	    n2 += 2.;
-	    t *= t2;
-	    cn /= n2 * (n2 + 1.);
-	    c[n] = cn;
-        let mut s: f64 = 0.;
-        if (n > 0) {
-            let mut coef = b - np1;
-            for i in 1..=n {
-                s += coef * c[i - 1] * d[n - i];
-                coef += b;
-            }
-        }
-        d[n] = (b-1.) * cn + s / np1;
-        sum += (d[n] * j);
-
-
-    }
-
-    logspace_add(w, log_u+sum.ln())
-}
-
-//Panics: x not an element of (0,1)
-fn modified_brcmp(a: f64, x: f64) -> f64 {
-
-    let a0 = 0.5_f64.min(a);
-    let b0 = 0.5_f64.max(a);
-
-
-
-    let lnx = x.ln();
-    let ln1mx = (-x).ln_1p();
- 
-    
-    let ans = a*lnx+0.5*ln1mx-ln_beta_with_3_halfs(a);
-
-    ans+a0.ln()-(a0/b0).ln_1p()
-
-    //implemented
-
-}
 
 fn ln_beta(a: f64, b:f64) -> f64 {
     ln_gamma(a+1.)+ln_gamma(b+1.)-ln_gamma(a+b+1.)
 }
 
-fn ln_beta_with_3_halfs(a: f64) -> f64 {
-    LN_GAMMA_3_HALFS+ln_gamma(a+1.)-ln_gamma(a+1.5)
+fn ln_beta_half_times_a(a: f64) -> f64 {
+    LN_GAMMA_HALF+ln_gamma(a+1.)-ln_gamma(a+0.5)
 }
 
 fn grat_r(x: f64, log_r: f64) -> f64{
@@ -476,13 +335,18 @@ mod tests{
     #[test]
     fn attempt() {
 
-        let dis = BackgroundDist::new(1.0, 2.84);
+        let dis = BackgroundDist::new(0.25, 2.84);
 
-        let data = [-0.3, 0.3, 4.5, -2.9];
+        let data = [2.2291285570e+0,  4.1510439540e-1, -5.8846691870e-1, -1.4300281780e+0, -1.0174699610e+0,  9.6801737090e-1,  6.1547874390e-1,  1.0320017990e+0, 9.8058816140e-1, 5.7552069380e-1];
 
-        for dat in data.iter() {
+        let cd = [-0.001841144352630861, -0.1056418605108091, -2.948328285560595, -5.091991745450134, -4.210455061116229, -0.01694448425305765, -0.04893062330685381, -0.01442399294418845, -0.0164061855049808, -0.05643039312469962];
+
+        let sf = [-6.298288400836528, -2.300056544954666, -0.05385159158458796, -0.006164729845405886, -0.01495082250690833, -4.086273189920248, -3.041717390080652, -4.246065609924472, -4.118288727978808, -2.902829897631842];
+
+        for (i, dat) in data.iter().enumerate() {
             let (c, s) = dis.ln_cd_and_sf(*dat);
-            println!("cd {}, sf {}",c, s);
+            println!("cd {}, sf {}, prop_cd {}, prop_sf {}, diff cd {}, diff sf {}",c, s, cd[i], sf[i], c-cd[i], s-sf[i]);
+            assert!(((c-cd[i]).abs() < 1e-10) && ((s-sf[i]).abs() < 1e-10));
         }
 
 
