acd2a1a48555e02c10a65706b11649b730c323f5
Ali Mohamad Farhat <alimf@login01.expanse.sdsc.edu>
2024-12-27 11:52:06 -0800

    Ensure sync

--- src/bin/process.rs
+++ src/bin/process.rs
@@ -109,7 +109,9 @@ pub fn main() {
         let regex = Regex::new(&(base_str.clone()+format!("_{}_trace_from_step_", min_chain).as_str()+"\\d{7}.bin")).unwrap();
         //let regex = Regex::new(&(base_str.clone()+"_trace_from_step_"+"0\\d{6}.bin")).unwrap();
         let directory_iter = fs::read_dir(&out_dir).expect("This directory either doesn't exist, you're not allowed to touch it, or isn't a directory at all!");
-        
+       
+        println!("try"); 
+
         let mut chain_files = directory_iter.filter(|a| regex.is_match(a.as_ref().unwrap().path().to_str().unwrap())).map(|a| a.unwrap().path().to_str().unwrap().to_string()).collect::<Vec<_>>();
 
         chain_files.sort();
